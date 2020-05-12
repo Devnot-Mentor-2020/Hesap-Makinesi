cc9e428daaa3e74c926eb35cb3e61e8ff71338a6
furkan <furkan9708@gmail.com>
2020-05-12 18:14:24 +0300

    Boş Dart projesi

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:hesap_makinesi/hesap_makinesi.dart' as hesap_makinesi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hesap_makinesi.calculate()}!');
+}
---
+++ test/hesap_makinesi_test.dart
@@ -0,0 +1,8 @@
+import 'package:hesap_makinesi/hesap_makinesi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
